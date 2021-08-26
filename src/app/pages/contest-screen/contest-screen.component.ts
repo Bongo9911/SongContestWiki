2a7b4bf7916f419d8fd4462b80219406412585e6
Bongo9911 <dawsonbuist@gmail.com>
2021-08-26 14:16:56 -0400

    started switch to Cloud Storage for images

--- src/app/pages/contest-screen/contest-screen.component.ts
+++ src/app/pages/contest-screen/contest-screen.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from "@angular/fire/firestore";
 import { Router, ActivatedRoute } from '@angular/router';
 import { Contest, Edition, Song } from 'src/app/shared/datatypes';
+import { AngularFireStorage } from '@angular/fire/storage'
 
 @Component({
   selector: 'app-contest-screen',
@@ -21,7 +22,10 @@ export class ContestScreenComponent implements OnInit {
   winners: Song[] = [];
   winnderEds: Edition[] = [];
 
-  constructor(private database: AngularFirestore, private router: Router, private route: ActivatedRoute) {
+  logos: string[] = []
+
+  constructor(private database: AngularFirestore, storage: AngularFireStorage, private router: Router,
+    private route: ActivatedRoute) {
     this.route.params.subscribe(params => this.id = params.id);
 
     this.database.firestore.collection('contests').doc(this.id)
@@ -36,7 +40,14 @@ export class ContestScreenComponent implements OnInit {
           this.eds.push(doc.data() as Edition);
           console.log(doc.data());
         });
-        this.eds.sort((a,b) => a.edval > b.edval ? 1 : -1);
+        this.eds.sort((a, b) => a.edval > b.edval ? 1 : -1);
+        this.logos = new Array(this.eds.length);
+        for (let i = 0; i < this.eds.length; ++i) {
+          storage.storage.ref('contests/' + this.id + '/logos/' + this.eds[i].edition + '.png')
+            .getDownloadURL().then(url => {
+              this.logos[i] = url;
+            })
+        }
       });
 
     this.database.firestore.collection('contests').doc(this.id).collection('newsongs')
@@ -44,7 +55,7 @@ export class ContestScreenComponent implements OnInit {
         docs.forEach(doc => {
           this.winners.push(doc.data() as Song)
         })
-        this.winners.sort((a,b) => a.edval > b.edval ? 1 : -1);
+        this.winners.sort((a, b) => a.edval > b.edval ? 1 : -1);
         this.winners.forEach(w => {
           this.winnderEds.push(this.eds.filter(ed => ed.edition == w.edition)[0])
         })
