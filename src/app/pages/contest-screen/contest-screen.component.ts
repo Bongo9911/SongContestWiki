845eb4f8e0f74ff133fc4affc26d4e152fd338bb
Bongo9911 <dawsonbuist@gmail.com>
2021-08-26 21:36:33 -0400

    fully transfered contest page over to using cloud storage

--- src/app/pages/contest-screen/contest-screen.component.ts
+++ src/app/pages/contest-screen/contest-screen.component.ts
@@ -16,13 +16,14 @@ export class ContestScreenComponent implements OnInit {
     name: '',
     id: ''
   };
-  eds: Edition[] = [];
+  eds: EditionWithUrl[] = [];
   id: string;
 
   winners: SongWithUrl[] = [];
-  winnerEds: Edition[] = [];
+  winnerEds: EditionWithUrl[] = [];
 
   logos: string[] = []; //For edition logo urls
+  edflags: string[][] = []; //Flags for each ed host
   mainLogo: string = ""; //For the contest logo
 
   constructor(private database: AngularFirestore, storage: AngularFireStorage, private router: Router,
@@ -43,16 +44,25 @@ export class ContestScreenComponent implements OnInit {
     this.database.firestore.collection('contests').doc(this.id).collection('editions').get()
       .then(docs => {
         docs.forEach((doc) => {
-          this.eds.push(doc.data() as Edition);
+          this.eds.push(doc.data() as EditionWithUrl);
           console.log(doc.data());
         });
         this.eds.sort((a, b) => a.edval > b.edval ? 1 : -1);
         this.logos = new Array(this.eds.length);
+        this.edflags = new Array(this.eds.length)
         for (let i = 0; i < this.eds.length; ++i) {
           storage.storage.ref('contests/' + this.id + '/logos/' + this.eds[i].edition + '.png')
             .getDownloadURL().then(url => {
               this.logos[i] = url;
             })
+          this.eds[i].flagurls = new Array(this.eds[i].hostcountries.length);
+          for (let j = 0; j < this.eds[i].flagurls.length; ++j) {
+            storage.storage.ref('contests/' + this.id + '/flags/' + this.eds[i].hostcountries[j]
+              + ' Flag.png')
+              .getDownloadURL().then(url => {
+                this.eds[i].flagurls[j] = url;
+              })
+          }
         }
       });
 
@@ -73,6 +83,17 @@ export class ContestScreenComponent implements OnInit {
         this.winners.forEach(w => {
           this.winnerEds.push(this.eds.filter(ed => ed.edition == w.edition)[0])
         })
+
+        // for (let i = 0; i < this.winnerEds.length; ++i) {
+        //   this.winnerEds[i].flagurls = new Array(this.winnerEds[i].hostcountries.length);
+        //   for (let j = 0; j < this.winnerEds[i].flagurls.length; ++j) {
+        //     storage.storage.ref('contests/' + this.id + '/flags/' + this.winnerEds[i].hostcountries[j]
+        //       + ' Flag.png')
+        //       .getDownloadURL().then(url => {
+        //         this.winnerEds[i].flagurls[j] = url;
+        //       })
+        //   }
+        // }
       })
   }
 
@@ -90,4 +111,11 @@ interface SongWithUrl {
   language: string;
   song: string;
   user: string;
+}
+
+interface EditionWithUrl {
+	edition: string;
+	edval: number;
+  hostcountries: string[];
+  flagurls: string[];
 }
